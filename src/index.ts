7b71e87dfd2060b4520a7bffcfab08cfc64807b6
Bruno Lemos <brunohplemos@gmail.com>
2018-06-29 23:07:47 -0300

    Send typing event when someone type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { RTMClient } from '@slack/client'
+
+const token = process.env.SLACK_TOKEN
+
+if (!token) throw new Error('[ENV] Missing environment variable: SLACK_TOKEN')
+
+const rtm = new RTMClient(token)
+rtm.start()
+
+rtm.on('user_typing', ({ channel }) => {
+  rtm.sendTyping(channel)
+})
+
 export default function() {
-  return 'Hello World'
+  return `${rtm.connected}`
 }

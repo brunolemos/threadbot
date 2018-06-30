8cd7f0389fad736c87023e4638e5d7ed993f621d
Bruno Lemos <brunohplemos@gmail.com>
2018-06-30 04:53:41 -0300

    New endpoint: /oauth

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,71 @@
-import { RTMClient } from '@slack/client'
+import { send } from 'micro'
+import { get, router } from 'microrouter'
 
-const token = process.env.SLACK_TOKEN
+import { RTMClient, WebAPICallResult, WebClient } from '@slack/client'
 
+const clientId = process.env.SLACK_CLIENT_ID!
+const clientSecret = process.env.SLACK_CLIENT_SECRET!
+const token = process.env.SLACK_TOKEN!
+
+if (!clientId) throw new Error('[ENV] Missing environment variable: SLACK_CLIENT_ID')
+if (!clientSecret) throw new Error('[ENV] Missing environment variable: SLACK_CLIENT_SECRET')
 if (!token) throw new Error('[ENV] Missing environment variable: SLACK_TOKEN')
 
 const rtm = new RTMClient(token)
+const web = new WebClient()
+
 rtm.start()
 
 rtm.on('user_typing', ({ channel }) => {
   rtm.sendTyping(channel)
 })
 
-export default function() {
-  return `${rtm.connected}`
-}
+export default router(
+  get('/', (_req, res) => send(res, 200, { ok: `${rtm.connected}` })),
+
+  get('/oauth', async (req, res) => {
+    const { code, error } = req.query
+
+    if (error) {
+      send(res, 400, { ok: false, message: error })
+      return
+    }
+
+    if (!code) {
+      send(res, 400, { ok: false, message: 'Missing required paremeter: code' })
+      return
+    }
+
+    let response
+    try {
+      response = await web.oauth.access({
+        code,
+        client_id: clientId,
+        client_secret: clientSecret,
+      })
+      if (!(response && response.ok)) throw new Error('Invalid response')
+    } catch (error) {
+      console.error(error)
+      send(res, 500, { ok: false, ...(error.data || error) })
+      return
+    }
+
+    const {
+      // access_token,
+      scope,
+      user_id,
+      team_name,
+      team_id,
+      bot: { bot_user_id /* bot_access_token */ },
+      scopes,
+    } = (response || {}) as WebAPICallResult & Record<string, any>
+
+    send(res, 200, { ok: true, bot_user_id, scope, scopes, team_id, team_name, user_id })
+
+    // res.setHeader(
+    //   'Location',
+    //   `https://slack.com/app_redirect?channel=general${team_id ? `&team=${team_id}` : ''}`,
+    // )
+    // send(res, 302)
+  }),
+)

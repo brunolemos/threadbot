25223acca928958dbd77c7e7683f1ecb1645532c
Bruno Lemos <brunohplemos@gmail.com>
2018-07-02 02:21:26 -0300

    Fix actions endpoint to handle multiple actions

--- src/routes/actions.ts
+++ src/routes/actions.ts
@@ -5,29 +5,31 @@ import { post } from 'microrouter'
 import { getParsedBody } from '../helpers'
 
 export default [
-  post('/actions/reply-use-thread', async (req, res) => {
+  post('/actions', async (req, res) => {
     const body = await getParsedBody(req, res)
     const payload = JSON.parse(body.payload)
 
     const userId = payload.message.user
 
-    await axios.post(payload.response_url, {
-      response_type: 'in_channel',
-      text: `${userId ? `<@${userId}> ` : ''}Please use thread instead 🙌`,
-      thread_ts: payload.message.ts,
-    })
+    switch (payload.callback_id) {
+      case 'start_thread': {
+        await axios.post(payload.response_url, {
+          response_type: 'in_channel',
+          text: `${userId ? `<@${userId}> ` : ''}Please use thread instead 🙌`,
+          thread_ts: payload.message.ts,
+        })
+        break
+      }
 
-    send(res, 200)
-  }),
-  post('/actions/start-thread', async (req, res) => {
-    const body = await getParsedBody(req, res)
-    const payload = JSON.parse(body.payload)
-
-    await axios.post(payload.response_url, {
-      response_type: 'in_channel',
-      text: '[THREAD] ⬇️',
-      thread_ts: payload.message.ts,
-    })
+      case 'use_thread': {
+        await axios.post(payload.response_url, {
+          response_type: 'in_channel',
+          text: '[THREAD] ⬇️',
+          thread_ts: payload.message.ts,
+        })
+        break
+      }
+    }
 
     send(res, 200)
   }),

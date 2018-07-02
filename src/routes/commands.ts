33ec5f45ad16e0fa4e7b0478a893c789055bf7dc
Bruno Lemos <brunohplemos@gmail.com>
2018-07-02 02:00:39 -0300

    New command: /thread, to start a thread

--- src/routes/commands.ts
+++ src/routes/commands.ts
@@ -5,6 +5,22 @@ import { post } from 'microrouter'
 import { getParsedBody } from '../helpers'
 
 export default [
+  post('/commands/thread', async (req, res) => {
+    const payload = await getParsedBody(req, res)
+
+    const text = `${payload.text || ''}`.trim()
+    if (!text) {
+      send(res, 200, 'A title is required to start a thread.')
+      return
+    }
+
+    await axios.post(payload.response_url, {
+      response_type: 'in_channel',
+      text: `[THREAD] <@${payload.user_id}>: ${text}`,
+    })
+
+    send(res, 200)
+  }),
   post('/commands/thread-reminder', async (req, res) => {
     const payload = await getParsedBody(req, res)
 
